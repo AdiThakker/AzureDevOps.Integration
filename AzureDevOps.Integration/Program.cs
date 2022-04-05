814fc7a4461c22a8b733a5998bdff937c97a83bd
Adi Thakker <Aditya.Thakker@gmail.com>
2022-04-05 17:37:04 -0400

    removed usings

--- AzureDevOps.Integration/Program.cs
+++ AzureDevOps.Integration/Program.cs
@@ -1,8 +1,5 @@
 ﻿using AzureDevOps.Integration;
 using Microsoft.Extensions.Configuration;
-using Microsoft.TeamFoundation.SourceControl.WebApi;
-using Microsoft.VisualStudio.Services.Common;
-using Microsoft.VisualStudio.Services.WebApi;
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Azure DevOps!");
